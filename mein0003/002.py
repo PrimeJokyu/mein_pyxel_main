e8f0da91493ad6724031437a7d03bc5d1d6e9b76
PrimeJokyu <primejokyu@gmail.com>
2025-10-19 13:54:27 +0900

    2025/10/19

---
+++ mein0003/002.py
@@ -0,0 +1,26 @@
+import pyxel
+
+ball_x=50
+ball_y=60
+dx = 2
+dy = 1
+
+pyxel.init(160, 120)
+
+def update():    
+    global ball_x, ball_y,dx,dy
+    ball_x += dx
+    ball_y += dy 
+   
+
+    if ball_x <= 0 or ball_x >= 160:
+        dx = -dx
+
+    if ball_y <= 0 or ball_y >= 120:
+        dy = -dy
+    
+def draw():
+    pyxel.cls(1)
+    pyxel.circ(ball_x, ball_y, 8, 10)
+
+pyxel.run(update,draw)
