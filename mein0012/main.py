65ca1de0ef8cc6805832837d9c2457dfad4452c5
PrimeJokyu <primejokyu@gmail.com>
2025-10-19 13:52:06 +0900

    10/19 oka key

--- mein0012/main.py
+++ mein0012/main.py
@@ -1,20 +1,38 @@
+
 import pyxel
 
-ball_x = 80
-ball_y = 60
-dx = 2
-dy = 1
+# 初期化
+pyxel.init(160, 120, title="Move + Jump Example")
 
-pyxel.init(160, 120)
+# プレイヤー情報
+player_x = 80
+player_y = 100
+color = 9
+
 def update():
+    global player_x, player_y
 
-    global ball_x, ball_y 
-    ball_x = ball_x + 1
-    ball_x += dx
-    ball_y += dy
+    base_speed = 2
+    turbo_speed = 5
+
+    # シフトキーで高速移動
+    if pyxel.btn(pyxel.KEY_SHIFT):
+        speed = turbo_speed
+    else:
+        speed = base_speed
+
+    # 4方向移動
+    if pyxel.btn(pyxel.KEY_LEFT):
+        player_x -= speed
+    if pyxel.btn(pyxel.KEY_RIGHT):
+        player_x += speed
+    if pyxel.btn(pyxel.KEY_UP):
+        player_y -= speed
+    if pyxel.btn(pyxel.KEY_DOWN):
+        player_y += speed
 
 def draw():
-    pyxel.cls(1)
-    pyxel.circ(ball_x, ball_y, 8, 10)
+    pyxel.cls(0)
+    pyxel.rect(player_x - 4, player_y - 4, 8, 8, color)
 
 pyxel.run(update, draw)
