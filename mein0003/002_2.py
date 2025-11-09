5304303e843e8e10676a1965d50fb9458a460158
PrimeJokyu <primejokyu@gmail.com>
2025-11-09 13:54:36 +0900

    2025-11-09

--- mein0003/002_2.py
+++ mein0003/002_2.py
@@ -8,16 +8,26 @@ dy = 2
 pyxel.init(160,120)
 
 def update():
-    global ball_x, dx
+    global ball_y
 
-    ball_x +=dx
-    if ball_x <=0 or ball_x >=160:
-        dx = -dx
+    time = pyxel.frame_count
 
-
+    if (time % 120) < 60:
+        ball_y += 1
+    else:
+        ball_y -= 1
+    
 def draw():
     pyxel.cls(1)
     pyxel.circ(ball_x,ball_y,8,10)
 
 pyxel.run(update,draw)
 
+import math
+def update():
+    global ball_x,ball_y
+
+    time = pyxel.frame_count * 0.1
+
+    ball_x =80 + math.cos(time) * 50
+    ball_y =60 + math.sin(time) * 30
