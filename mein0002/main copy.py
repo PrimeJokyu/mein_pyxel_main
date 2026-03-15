b075bc2a482c42f2768a9ff73cfb95e7b2327083
PrimeJokyu <primejokyu@gmail.com>
2026-03-15 13:51:12 +0900

    pyxel

--- mein0002/main copy.py
+++ mein0002/main copy.py
@@ -1,30 +1,6 @@
 import pyxel
-
-FPS = 60
-INTERVAL_SEC = 0.2
-INTERVAL_FRAMES = int(FPS * INTERVAL_SEC)  # 12
-
-class App:
-    def __init__(self):
-        pyxel.init(160, 120, fps=FPS)
-        pyxel.load("my_game.pyxres")  # 必要なら
-        self.anim = 0  # 0/1/2... のコマ番号
-        self.num_frames = 4  # アニメーション枚数（例）
-        pyxel.run(self.update, self.draw)
-
-    def update(self):
-        # 12フレームごとに anim を進める
-        self.anim = (pyxel.frame_count // INTERVAL_FRAMES) % self.num_frames
-
-    def draw(self):
-        pyxel.cls(0)
-
-        # 例：同じ行に 16x16 のスプライトが横並びで入っている想定
-        u = self.anim * 16
-        v = 0
-        pyxel.blt(72, 52, 0, u, v, 16, 16, 0)
-
-App()
+import math
+pyxel.init(160, 120)
 # 状態を辞書で管理（クラス不使用）
 character = {
     "x": 80,
@@ -39,8 +15,7 @@ character = {
         "attack": [(80, 0), (96, 0)]            # 2フレーム
     }
 }
-pyxel.init(160, 120)
-pyxel.load("my_game.pyxres")
+
 def update_character():
     c = character
     # アニメーションフレーム更新
@@ -64,7 +39,7 @@ def draw_character():
     frames = c["animations"][c["current_animation"]]
     frame_index = (c["animation_frame"] // c["animation_speed"]) % len(frames)
     sprite_x, sprite_y = frames[frame_index]
-    pyxel.blt(c["x"], c["y"], 0, sprite_x, sprite_y, 16, 16, 1)
+    pyxel.blt(c["x"], c["y"], 0, sprite_x, sprite_y, 16, 16, 0)
 
 # 使用例
 def update():
@@ -73,5 +48,4 @@ def update():
 def draw():
     pyxel.cls(12)
     draw_character()
-    
 pyxel.run(update, draw)
