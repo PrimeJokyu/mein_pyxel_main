d327a4cc8d2cb8e9626d0a64f6afc3d47ea06805
PrimeJokyu <primejokyu@gmail.com>
2026-04-04 14:53:37 +0900

    26.4.4

--- mein0003/003.py
+++ mein0003/003.py
@@ -2,7 +2,7 @@ import pyxel
 
 player_x = 80
 player_y = 60
-jump_count = 0
+jump_timer = 0
 color = 8
 
 pyxel.init(160,120)
@@ -11,7 +11,7 @@ def update():
     global player_x,player_y, player_color, player_size
 
     base_speed = 2
-    turbo_speed = 5
+    turbo_speed = 4
     dx = 0
     dy = 0
 
@@ -20,17 +20,24 @@ def update():
     if pyxel.btn(pyxel.KEY_RIGHT):
         player_x += base_speed
     if pyxel.btn(pyxel.KEY_UP):
-        player_y -= speed
+        player_y -= base_speed
     if pyxel.btn(pyxel.KEY_DOWN):
-        player_y += speed
+        player_y += base_speed
+
     if pyxel.btn(pyxel.KEY_A):
         player_x -= turbo_speed
     if pyxel.btn(pyxel.KEY_D):
         player_x += turbo_speed
-    if pyxel.btn(pyxel.KEY_W)
+    if pyxel.btn(pyxel.KEY_W):
         player_y -= turbo_speed
-    if pyxel.btn(pyxel.KEY_S)
+    if pyxel.btn(pyxel.KEY_S):
         player_y += turbo_speed
+    
+    #if pyxel.btnp(pyxel.KEY_SPACE):
+     #   jump_timer = 10
+
+    #if jump_timer > 0:
+      #  jump_timer -= 1
     
     player_x = max(0, min(player_x, 160))
     player_y = max(0, min(player_y, 120))
