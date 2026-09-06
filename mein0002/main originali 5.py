5bc2b7919fa28f1613a6cf2afe894c299f722e91
PrimeJokyu <primejokyu@gmail.com>
2026-09-06 14:04:15 +0900

    pyxel

--- mein0002/main originali 5.py
+++ mein0002/main originali 5.py
@@ -2,41 +2,99 @@
 import pyxel
 pyxel.init(160, 120)
 pyxel.load("my_game2.pyxres")
+enemy_type = {
+    1:{
+        "name": "スライム",
+        "hp": 100,
+        "attack": 10,
+        "defense": 10,
+        "enemy_x": 0,
+        "enemy_y": 0,
+    },
+    2:{
+        "name": "ドラキー",
+        "hp": 150,
+        "attack": 15,
+        "defense": 5,
+        "enemy_x": 16,
+        "enemy_y": 0,
+    },
+}
+e = 1
 
+frame_counter = 0
 screen = 1
 looks_x = 0
 looks_y = 0
 looks_slippage_y = 0
 player_x = 80
 player_y = 60
+work_counter = 0
+work_count = pyxel.rndi(150, 200)
+
 def update_waiting():
-    global screen,player_x,player_y,looks_x,looks_y,looks_slippage_y
+    global screen,player_x,player_y,looks_x,looks_y,looks_slippage_y,frame_counter,work_counter
+
+    frame_counter += 1
+
+    if frame_counter == 15:
+        frame_counter = 0
+        if looks_slippage_y == 0:
+            looks_slippage_y = 1
+        elif looks_slippage_y == 1:
+            looks_slippage_y = 0
+
     if pyxel.btn(pyxel.KEY_UP):
         player_y -= 2
-        looks_x =0
-        looks_y =0
+        looks_y =32
 
     if pyxel.btn(pyxel.KEY_DOWN):
         player_y += 2
+        looks_y =48
+
     if pyxel.btn(pyxel.KEY_LEFT):
         player_x -= 2
+        looks_y =16
+
     if pyxel.btn(pyxel.KEY_RIGHT):
         player_x += 2
+        looks_y =0
+
+    if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.KEY_DOWN):
+        work_counter += 1
+        if work_counter == work_count:
+            screen = 2
+            work_counter = 0
+
+def update_battle_go():
+    global screen,frame_counter
+    frame_counter += 1
+    if frame_counter == 100:
+        screen = 3
+        frame_counter = 0
 
 def update_battle():
     pass
 
 def draw_waiting():
-    pyxel.blt(player_x, player_y, 0, 0, 0, 16, 16,1)
+    pyxel.blt(0, 0, 2, 0, 0, 160, 120,)
+    pyxel.blt(player_x, player_y, 0, 0, looks_y+looks_slippage_y, 16, 16 ,1)
     
 
+def draw_battle_go():
+    pyxel.cls(0)
+
 def draw_battle():
-    pass
+    pyxel.blt(enemy_type[e]["enemy_x"], enemy_type[e]["enemy_y"], 1, 0, 16, 16, 16)
+    pyxel.blt(player_x, player_y, 0, 0, looks_y+looks_slippage_y, 16, 16 ,1)
+    pyxel.blt(0, 0, 2, 0, 120, 160, 120,)
 
 def update():
     if screen == 1:
         update_waiting()
     elif screen == 2:
+        update_battle_go()
+    elif screen == 3:
         update_battle()
 
 def draw():
@@ -44,7 +102,12 @@ def draw():
     if screen == 1:
         draw_waiting()
     elif screen == 2:
+        draw_battle_go()
+    elif screen == 3:
         draw_battle()
+
+    pyxel.text(5, 5, str(work_counter), 15)
+    pyxel.text(5, 15, str(work_count), 15)
 
 
 pyxel.run(update, draw)
