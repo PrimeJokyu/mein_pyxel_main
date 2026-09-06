b82678bf06e45f4162272fadf7ecbf9208ae46c9
PrimeJokyu <primejokyu@gmail.com>
2026-09-06 15:02:31 +0900

    feat: initialize Pyxel environment and implement basic drawing functions for main and level 2 scripts

--- mein0014/main.py
+++ mein0014/main.py
@@ -1,3 +1,4 @@
+from pyxel import cls
 import pyxel
 #あいうえお
 pyxel.init(100,100)
@@ -11,6 +12,17 @@ def draw():
     pyxel.circb(10,10,5,9)
     pyxel.tri(20,20,20,20,20,20,5)
     pyxel.trib(20,20,20,20,20,20,5)
+
+    pyxel.init(160,120)
+
+def update():
+    pass
+
+def drow():
+    pyxel.cls(12)#青い背景
+
+    #顔（ピーチ色の円）
+
     
 
 pyxel.run(update,draw)
